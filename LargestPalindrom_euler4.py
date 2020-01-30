d44caf4a8da2fe5a04d4cc3ceb18c49e0db10106
Deb001 <debudas@gmail.com>
2020-01-30 06:10:11 +0530

    Update LargestPalindrom_euler4.py

--- LargestPalindrom_euler4.py
+++ LargestPalindrom_euler4.py
@@ -14,32 +14,10 @@ def isPalindrom(num):
     else:
         return 0
 
-# checks if the number is a prime. 
-def isPrime(num):
-    val = (int) (math.sqrt(num))
-    for i in range(2,val +1):
-        if num % i == 0:
-            return 0
-    return 1
-
-def hasDivisor(num,base):
-    while (base > 100):
-        if (num % base == 0):# check if it is a 2 digit number
-            if (num/base < 999):
-                return 1
-        base = base -1
-    return 0
-
-
 largeNum = 999 * 999
 lowNum = 100 * 100
-#palindrom = []
 while (largeNum > lowNum):
     if (isPalindrom(largeNum)):
-        if(isPrime(largeNum) == 0):
-#            palindrom.append(largeNum)
-            if (hasDivisor(largeNum,999)):
-                break
+        break
     largeNum -=1
 print(largeNum)
-#print(palindrom)
